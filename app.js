da0f845b75905a43472bd17962dc238388d4a30d
Igor <iandrosov73@gmail.com>
2019-09-24 09:28:55 +0900

    Update auth code

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express')
 const https = require('https')
 
+var curl = require('curlrequest');
+
 const app = express()
 const port = 5000
 var path = __dirname + '/views/';
@@ -22,6 +24,107 @@ let SF_DEV_LOGIN = 'https://login.salesforce.com';
 let SF_SANDBOX_LOGIN = 'https://test.salesforce.com';
 
 //app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/ai', (req, res) => {
+/*
+	var options = {
+  host: 'https://api.einstein.ai/v2/language',
+  //port: 80,
+  path: '/sentiment',
+  method: 'POST',
+  headers: {
+  			'Authorization': 'Bearer 2Y22M3W3DQMXD3HOMF22PRMWIFFVSJLT4NJHCFEECL7AHJR5ZVOCRALO3RMNVIY2HHAOBQR3TRZ4HMZEYHVVLQYOVVCTFE34W4WRNEA',
+	    		'Content-Type': 'multipart/form-data',
+	    		'Cache-Control': 'no-cache'
+	       }  
+	};
+
+**/
+	var formData = {
+    	modelId: 'CommunitySentiment',
+    	document: 'the presentation was great and I learned a lot'
+	}
+
+	const postData = "{modelId: 'CommunitySentiment', document: 'the presentation was great and I learned a lot'}";
+
+	var options = { url: 'https://api.einstein.ai',
+		prototcol: 'https',
+	    method: 'POST',
+	    path: '/v2/language/sentiment',
+		headers: {
+  			'Authorization': 'Bearer P6IH6NM56WPVQVEEBPI2Z6P4RWD356D7W6FARTGU3TXMEYIA3JU7FGPNNQDBESZQNDHSD2KUWIB23X2NVU7N3FD6WYV5BBWTSJCUIXI',
+	    		'Content-Type': 'multipart/form-data',
+	    		'Cache-Control': 'no-cache'
+	       }
+		};
+
+
+		const auth_req = https.request(options, (auth_res) => {
+	  		console.log('statusCode: ${auth_res.statusCode}')
+
+	  		auth_res.on('data', (d) => {
+	    		process.stdout.write(d)
+	  		})
+		})
+
+		auth_req.on('error', (error) => {
+	  		console.error(error)
+		})
+
+		auth_req.write(postData)
+		auth_req.end()
+
+	    //data: {'modelId': 'CommunitySentiment', 'document': 'the presentation was great and I learned a lot'}  
+	    //data: 'modelId=CommunitySentiment&document=the presentation was great and I learned a lot',
+
+/*
+	var request = https.request(options, function (rest) {
+	    var responseString = "";
+
+	    rest.on("data", function (data) {
+	        responseString += data;
+	        // save all the data from response
+	    });
+	    rest.on("end", function () {
+	        console.log(responseString); 
+	        // print to console when response ends
+	    });
+	});	    
+
+	request.write(postData);
+	request.end();
+*/
+
+/*
+	curl.request(options, function (err, parts) {
+    	parts = parts.split('\r\n');
+    	var data = parts.pop()
+      		, head = parts.pop();
+      	console.log('ERROR:' + err);	
+    	console.log('RESULT: ' + data);  
+	});
+*/	
+/*
+	const auth_req = https.request(options, function(rest) {
+  		console.log('STATUS: ' + rest.statusCode);
+  		console.log('HEADERS: ' + JSON.stringify(rest.headers));
+  		rest.setEncoding('utf8');
+  		rest.on('data', function (chunk) {
+    		console.log('BODY: ' + chunk);
+  		});
+	}).end();
+*/
+/*
+	const auth_req = https.request(options, (aires) => {
+	  		console.log(aires);
+
+	  		res.send('Hello World!'+aires);
+		});
+	auth_req.on('error', (error) => {
+	  		console.error(error)
+	});
+	*/
+})
 
 app.get('/', (req, res) => res.sendFile(path + "index.html"))
 
